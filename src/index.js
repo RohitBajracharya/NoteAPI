4a94c86c5bd70cac5c41dd708911fef86f61d01d
RohitBajra <104704874+rohitnewgit@users.noreply.github.com>
2024-03-22 20:32:04 +0545

    env key used, and next() put inside if block

--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ const express = require("express")
 const userRouter = require("./routes/userRoutes");
 const noteRouter = require("./routes/noteRoutes");
 const mongoose = require("mongoose");
+
+
 const app = express();
 dotenv.config()
 
--- src/middlewares/auth.js
+++ src/middlewares/auth.js
@@ -1,6 +1,7 @@
+const dotenv = require("dotenv")
 const jwt = require("jsonwebtoken");
 
-const SECRET_KEY = "NOTESAPI";
+const SECRET_KEY = process.env.SECRET_KEY;
 
 const auth = (req, res, next) => {
     try {
@@ -9,14 +10,13 @@ const auth = (req, res, next) => {
             token = token.split(" ")[1]
             let user = jwt.verify(token, SECRET_KEY);
             req.userId = user.id;
+            next();
         } else {
             res.status(401).json({ message: "Unauthorized User" })
         }
-        next();
     } catch (error) {
         console.log(error);
         res.status(401).json({ message: "Unauthorized User" })
-
     }
 }
 
